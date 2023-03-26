7208007b4ad933cf92e70c595b8a6543c296864f
Imran Khan <imransilvake@gmail.com>
2023-03-26 14:42:54 +0200

    chore: first commit

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import './style.css';
+import './../public/assets/scss/main.scss';
+
+import { createApp } from 'vue';
+
+import App from './app/App.vue';
+
+createApp(App).mount('#app');
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import vue from '@vitejs/plugin-vue';
+import { defineConfig, loadEnv } from 'vite';
+import { configDefaults } from 'vitest/config';
+
+const configuration = defineConfig(({ mode }) => {
+	// load env file based on `mode` in the current working directory.
+	// set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+	const env = loadEnv(mode, process.cwd());
+
+	return {
+		plugins: [vue()],
+		logLevel: 'error',
+		server: { port: +env.VITE_PORT, open: false },
+		preview: { port: +env.VITE_PORT_PREVIEW, open: false },
+		build: { sourcemap: false },
+		test: {
+			globals: true,
+			exclude: [...configDefaults.exclude]
+		}
+	};
+});
+export default configuration;
